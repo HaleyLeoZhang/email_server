0c238625da1b8e5b662f2a8ace4c37e3d1931315
云天河 <hlzblog@vip.qq.com>
2021-03-12 23:12:26 +0800

    update

--- constant/rabbit_mq.go
+++ constant/rabbit_mq.go
@@ -1,7 +1,7 @@
 package constant
 
 const (
-	RABBIT_MQ_EXCHANGE   = "amqp.topic"    // 交换机名
+	RABBIT_MQ_EXCHANGE   = "amq.topic"    // 交换机名
 	RABBIT_MQ_ROUTIE_KEY = "email.sender"    // routineKey
 	RABBIT_MQ_QUEUE      = "email_sender" // 消费队列名
 )
